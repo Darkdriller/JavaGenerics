ec49a41a12f113dead30f756aa21170e29375c2d
Darkdriller <swaindhruv28@gmail.com>
2023-11-29 08:35:44 +0530

    Refactored: UC2 and UC3 added generic to method and class

--- src/main/java/Day8/PrintArray.java
+++ src/main/java/Day8/PrintArray.java
@@ -1,6 +1,11 @@
 package Day8;
 
-public class PrintArray {
+public class PrintArray <T> {
+
+    private T[]  Array;
+    public  PrintArray(T[] Array){
+        this.Array = Array;
+    }
     public static void toPrint(Integer[] inputArray) {
         for(int integer : inputArray) {
             System.out.print(integer + " ");
@@ -21,14 +26,24 @@ public class PrintArray {
         }
         System.out.println();
     }
+    public void toPrint(){
+        printElements(this.Array);
+    }
+    public static <T> void printElements(T[] dataArray) {
+        for (T element : dataArray) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
         Integer[] intArray = {100, 99, 98, 97, 96};
+        var intArrayPrintArray = new PrintArray(intArray);
         Double[] doubleArray = {3.14, 23.14, 0.412, 2.718, 1.618};
         Character[] charArray = {'D', 'H', 'R', 'U', 'V'};
 
-        PrintArray.toPrint(intArray);
-        PrintArray.toPrint(doubleArray);
-        PrintArray.toPrint(charArray);
+        intArrayPrintArray.toPrint();
+        printElements(doubleArray);
+        printElements(charArray);
     }
 }
