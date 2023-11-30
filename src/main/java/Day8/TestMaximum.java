5badc92c53b908294b5e4af52447ee9970eaf6c9
Darkdriller <swaindhruv28@gmail.com>
2023-11-30 16:13:44 +0530

    Removed: Commented Code

--- src/main/java/Day8/TestMaximum.java
+++ src/main/java/Day8/TestMaximum.java
@@ -1,37 +1,6 @@
 package Day8;
 
 public class TestMaximum<T extends Comparable<T>>{
-//    public static Integer testMaximum(Integer x, Integer y, Integer z) {
-//        Integer max = x;
-//        if (y.compareTo(max) > 0) {
-//            max = y;
-//        }
-//        if (z.compareTo(max) > 0) {
-//            max = z;
-//        }
-//        return max;
-//    }
-//    public static Float testMaximum(Float x, Float y, Float z) {
-//        Float max = x;
-//        if (y.compareTo(max) > 0) {
-//            max = y;
-//        }
-//        if (z.compareTo(max) > 0) {
-//            max = z;
-//        }
-//        return max;
-//    }
-//
-//    public static String testMaximum(String x, String y, String z) {
-//        String max = x;
-//        if (y.compareTo(max) > 0) {
-//            max = y;
-//        }
-//        if (z.compareTo(max) > 0) {
-//            max = z;
-//        }
-//        return max;
-//    }
     T x, y, z;
     public TestMaximum(T x, T y, T z) {
         this.x = x;
